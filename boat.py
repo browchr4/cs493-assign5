86e326375a5873374bb3b6b20485b1f3abe5bdc3
browchr4 <browchr4@oregonstate.edu>
2020-05-06 20:02:02 -0700

    covers all status codes

--- boat.py
+++ boat.py
@@ -1,81 +1,117 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, make_response, render_template_string
 from google.cloud import datastore
 import json
 import constants
+
+#used class lectures, previous assignment, https://stackoverflow.com/questions/49390075/how-do-i-convert-python-json-into-a-html-table-in-flask-server/49391508, https://stackoverflow.com/questions/49253142/check-response-headers-value-in-postman-tests
 
 client = datastore.Client()
 
 bp = Blueprint('boat', __name__, url_prefix='/boats')
 
-@bp.route('/', methods=['POST','GET'])
+@bp.route('/', methods=['POST'])
 def boats_get_post():
     if request.method == 'POST':
         content = request.get_json()
         if 'name' not in content.keys():
-            return("need a name", 400)
+            return({'Error': "need a name"}, 400)
         if 'type' not in content.keys():
-            return("need a type", 400)
+            return({'Error': "need a type"}, 400)
         if 'length' not in content.keys():
-            return("need a length", 400)
+            return({'Error': "need a length"}, 400)
+        query = client.query(kind=constants.boats)
+        allBoats = query.fetch()
+        for x in allBoats:
+            if x['name'] == content['name']:
+                return({'Error': "A boat with that name already exists"}, 403)
         new_boat = datastore.entity.Entity(key=client.key(constants.boats))
         new_boat.update({'name': content['name'], 'type': content['type'],
-          'length': content['length'], 'loads': []})
+          'length': content['length']})
         client.put(new_boat)
-        return str(new_boat.key.id)
-    elif request.method == 'GET':
-        query = client.query(kind=constants.boats)
-        q_limit = int(request.args.get('limit', '3'))
-        q_offset = int(request.args.get('offset', '0'))
-        l_iterator = query.fetch(limit= q_limit, offset=q_offset)
-        pages = l_iterator.pages
-        results = list(next(pages))
-        if l_iterator.next_page_token:
-            next_offset = q_offset + q_limit
-            next_url = request.base_url + "?limit=" + str(q_limit) + "&offset=" + str(next_offset)
-        else:
-            next_url = None
-        for e in results:
-            selfLink = request.host_url + 'boats/' + str(e.key.id)
-            e["self"] = selfLink
-        output = {"boats": results}
-        if next_url:
-            output["next"] = next_url
-        return json.dumps(output)
+        boat = client.get(key=new_boat.key)
+        if boat is None:
+            return ({'Error': 'Error creating'}, 204)
+        boat['id'] = new_boat.key.id
+        boat['self'] = request.host_url + '/boats/' + str(new_boat.key.id)
+        return (boat, 201)
     else:
-        return 'Method not recogonized'
+        return ('Method not recogonized', 405)
 
-@bp.route('/<id>', methods=['DELETE', 'GET'])
+@bp.route('/<id>', methods=['DELETE', 'GET', 'PUT', 'PATCH'])
 def boats_put_delete_get(id):
     if request.method == 'DELETE':
         boat_key = client.key(constants.boats, int(id))
         boat = client.get(key=boat_key)
         if boat is None:
-            return('Invalid boat ID', 400)
-        elif len(boat['loads']) == 0:
-            client.delete(boat_key)
-            return ('', 200)
+            return({'Error': 'No boat with this ID exists'}, 404)
         else:
-            for x in boat['loads']:
-                load_key = client.key(constants.loads, int(x))
-                load = client.get(key=load_key)
-                load.update({'owner': load['owner'], 'boat': '', 'weight': load['weight'], 'contents': load['contents']})
-                client.put(load)
             client.delete(boat_key)
-            return ('', 200)
+            return ('', 204)
     elif request.method == 'GET':
         boat_key = client.key(constants.boats, int(id))
         boat = client.get(key=boat_key)
         if boat is None:
-            return('Invalid boat ID', 400)
+            return('Invalid boat ID', 404)
         else:
-            output = []
-            for x in boat['loads']:
-                load_key = client.key(constants.loads, int(x))
-                load = client.get(key=load_key)
-                load['self'] = request.host_url + '/loads/' + str(x)
-                output.append(load)
-            boat['loads'] = output
-            return (boat, 200)
+            boat['id'] = id
+            boat['self'] = request.host_url + '/boats/' + str(id)
+            if 'text/html' in request.accept_mimetypes:
+                htmlBoat = render_template_string('''
+                <li>
+                    <ul>{{ boat.name }}</ul>
+                    <ul>{{ boat.type }}</ul>
+                    <ul>{{ boat.length }}</ul>
+                </li>
+                ''', boat=boat)
+                res = make_response(htmlBoat)
+                res.headers.set('Content-Type', 'text/html')
+                res.status_code = 200
+                return res
+            elif 'application/json' in request.accept_mimetypes:
+                return (boat, 200)
+            else:
+                return ({'Error': "Content-Type not supported"}, 406)
+    elif request.method == 'PUT':
+        content = request.get_json()
+        if 'name' not in content.keys():
+            return({'Error': "need a name"}, 400)
+        if 'type' not in content.keys():
+            return({'Error': "need a type"}, 400)
+        if 'length' not in content.keys():
+            return({'Error': "need a length"}, 400)
+        query = client.query(kind=constants.boats)
+        allBoats = query.fetch()
+        for x in allBoats:
+            if x['name'] == content['name']:
+                return({'Error': "A boat with that name already exists"}, 403)
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key)
+        if boat is None:
+            return('There is no boat by that ID', 404)
+        else:
+            boat['name'] = content['name']
+            boat['type'] = content['type']
+            boat['length'] = content['length']
+            client.put(boat)
+            boat['self'] = request.host_url + 'boats/' + str(id)
+            res = make_response('')
+            res.headers.set('Location', boat['self'])
+            res.status_code = 303
+            return res
+    elif request.method == 'PATCH':
+        content = request.get_json()
+        boat_key = client.key(constants.boats, int(id))
+        boat = client.get(key=boat_key)
+        if boat is None:
+            return('There is no boat by that ID', 404)
+        if 'name' in content.keys():
+            boat['name'] = content['name']
+        if 'type' in content.keys():
+            boat['type'] = content['type']
+        if 'length' in content.keys():
+            boat['length'] = content['length']
+        client.put(boat)
+        boat['self'] = request.host_url + 'boats/' + str(id)
+        return (boat['self'], 200)
     else:
-        return 'Method not recogonized'
-
+        return ('Method not recogonized', 405)
